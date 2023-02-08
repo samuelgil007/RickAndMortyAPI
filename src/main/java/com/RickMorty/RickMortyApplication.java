4c765eb63f3be67bc4e58d3db6e459db57965369
samuelgil007 <sagilar15@gmail.com>
2023-02-07 19:00:28 -0500

    Feature: project base

---
+++ src/main/java/com/RickMorty/RickMortyApplication.java
@@ -0,0 +1,13 @@
+package com.RickMorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickMortyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RickMorty/RickMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RickMorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickMortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
